ac3d5910e74cda7c3e7e54e2b3fc22731f177922
bimapw4 <bimapw4@gmail.com>
2022-08-17 11:28:00 +0700

    pertanyaan balapan kuda

---
+++ kuda.js
@@ -0,0 +1,15 @@
+/**
+ * Soal :
+ * Ada 25 kuda dengan berkecapatan berbeda, anda hanya bisa melihat perbedaan kecepatan kuda dengan mengadakan balapan kuda dan 
+ * hanya bisa mengikut sertakan 5 kuda dalam 1 balapan. Anda tidak punya penghitung waktu, 
+ * tapi anda bisa mengetahui dari mata Anda dalam 1 balapan siapa yang juara 1 sampai 5. 
+ * Berapa banyak balapan yang anda harus gelarkan untuk mencari 3 kuda tercepat?
+ * 
+ * 
+ * jawaban nya 6 kali balapan,
+ * alasan:
+ * Karena setiap balapan ada 5 kuda yang dapat di ikut sertakan jadi kalau punya kuda 25 maka bisa 25 / 5 = 5
+ * kemudian jika setiap balapan kuda tersebut kita ambil hanya juara satu saja maka kita akan memiliki 5 calon kandidat baru
+ * Kelima kuda juara satu tersebut dapat kita adu pada balapan putaran ke 6 maka akan terlihat kuda 3 kuda tercepat
+ * 
+ */
